524e82c75d3ad35dbd9d8f7ccc75f3b9882efc03
cbyce <cbyce@uwo.ca>
2021-10-19 13:17:13 -0400

    Trying things and deleted uneccessary files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+function BoardCountingButton(props)
+{
+  const [count, setCount] = useState(0);
+  return (
+    <button id='props.id' onClick={() => {setCount(count + 1)}}>
+      This button was clicked {count} times.
+    </button>
+  );
+}
+class BoardButton extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {count: 0};
+  }
+  render() 
+  {
+    return (
+      <button id={this.props.id} onClick={() => this.setState({count: (++this.state.count)})}>
+        This button was clicked {this.state.count} times
+      </button>
+    );
+  }
+
+  componentDidMount() 
+  {
+    //alert("The button was just introduced ==>" + document.getElementById('btn').getBoundingClientRect().top);
+  }
+
+  componentDidUpdate()
+  {
+    //alert("This button has updated")
+  }
+}
+
+class Slot extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {'switch': 'OFF'};
+  }
+  render() 
+  {
+    return (
+      <button id={this.props.id} onClick= {() => 
+                                            {
+                                              let newState = (this.state.switch === 'OFF')? 'ON' : 'OFF';
+                                              this.setState({'switch' : newState});
+
+                                            }
+                                          } >
+        {this.state.switch}
+      </button>
+    );
+  }
+}
+class GameBoard extends React.Component
+{
+  render() 
+  {
+    return (
+      <div>
+        <Slot />
+        <Slot />
+        <Slot />
+        <Slot />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <GameBoard />
+    {/* <BoardCountingButton id='countBtn'/>
+    <br/>
+    <BoardButton id='btn'/>
+    <br/>
+    <BoardButton id='btnTwo'/> */}
+  </div>
+  ,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
