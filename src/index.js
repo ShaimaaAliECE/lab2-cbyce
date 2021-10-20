593be7528389773c5ccde62a41b1246c17180e74
cbyce <cbyce@uwo.ca>
2021-10-19 20:01:00 -0400

    Added styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+
+const boardStyle = {
+  backgroundColor: 'blue',
+  borderRadius: '5px',
+  width: '420px', //Must maintain hole size below
+
+};
+
+const pageStyle = {
+  display: 'flex',
+  alignItems: 'center', 
+  justifyContent: 'space-evenly', 
+  flexDirection: 'column',
+  height: '100vh'
+}
+
 
 function Hole(props)
 {
+  const holesStyle = {
+    backgroundColor: props.owner,
+    height: '50px',
+    width: '50px',
+    borderRadius: '25px',
+    margin: '5px'
+  };
+  
   return (
-    <button id={props.id} onClick={props.onClick} >
+    <button style={holesStyle} id={props.id} onClick={props.onClick} >
       {props.owner}
     </button>
   );
@@ -17,9 +42,10 @@ class GameBoard extends React.Component
     super(props);
 
     this.state = {
-      gameBoard: new Array(6).fill(new Array(7).fill('Empty')),
+      gameBoard: new Array(6).fill(new Array(7).fill('white')),
       winner: '',
-      playersTurn: 'Red',
+      count: 0,
+      playersTurn: 'red', //Game starts with red player 
 
     }
   }
@@ -27,9 +53,10 @@ class GameBoard extends React.Component
   renderHole(posX, posY)
   {
     return (
-      <Hole id={'X' + posX + 'Y' + posY} owner={this.state.gameBoard[posY][posX]} onClick={() => 
+      <Hole  id={'X' + posX + 'Y' + posY} owner={this.state.gameBoard[posY][posX]} onClick={() => 
                                            {
-                                             if(this.state.gameBoard[posY][posX] === 'Empty') {
+                                              if(this.state.gameBoard[posY][posX] === 'white') {
+                                                console.log(this.state.count);
                                                 const newBoard = this.state.gameBoard.map((arr) => {
                                                   return arr.slice();
                                                 });
@@ -37,13 +64,14 @@ class GameBoard extends React.Component
                                                 let newVal = newBoard[posY];
 
                                                 newVal[posX] = this.state.playersTurn;
-                                                
+                                              
 
                                                 this.setState({
-                                                  playersTurn: (this.state.playersTurn === 'Red') ? 'Yellow' : 'Red',
-                                                  gameBoard: newBoard
+                                                  playersTurn: (this.state.playersTurn === 'red') ? 'yellow' : 'red',
+                                                  gameBoard: newBoard,
+                                                  count: (this.state.count + 1)
                                                 });
-                                              }
+                                              } 
                                             }
                                         }/>
     );
@@ -67,31 +95,31 @@ class GameBoard extends React.Component
   render() 
   {
     return (
-      <div>
+      <div style={pageStyle}>
         <div>{this.state.playersTurn} Players Turn</div>
-        {this.renderRow(0)}
-        {this.renderRow(1)}
-        {this.renderRow(2)}
-        {this.renderRow(3)}
-        {this.renderRow(4)}
-        {this.renderRow(5)}
+        <div style={boardStyle}>
+          {this.renderRow(0)}
+          {this.renderRow(1)}
+          {this.renderRow(2)}
+          {this.renderRow(3)}
+          {this.renderRow(4)}
+          {this.renderRow(5)}
+        </div>
       </div>
     );
   }
 
   componentDidUpdate()
   {
-    if(checkWinner(this.state.gameBoard))
-    {
-      let win = (this.state.playersTurn === 'Red') ? 'Yellow' : 'Red';
-    }
+    let win = checkWinner(this.state.gameBoard);
+    console.log(win);  
   }
 }
 
 function checkFour(a, b, c, d)
 {
   return (
-          (a !== 'Empty' && b !== 'Empty' && c !== 'Empty' && d !== 'Empty') 
+          (a !== 'white' && b !== 'white' && c !== 'white' && d !== 'white') 
           && 
           (a === b && b === c && c === d)
     );
@@ -105,7 +133,7 @@ function checkWinner(b)
     {
       if (checkFour(b[j][0 + i], b[j][1 + i], b[j][2 + i], b[j][3 + i]))
       {
-        return true;
+        return b[j][0 + i];
       }
     }
   }
@@ -116,12 +144,12 @@ function checkWinner(b)
     {
       if (checkFour(b[0 + i][j], b[1 + i][j], b[2 + i][j], b[3 + i][j]))
       {
-        return true;
+        return b[0 + i][j];
       }
     }
   }
 
-  return false;
+  return '';
 }
 
 ReactDOM.render(
